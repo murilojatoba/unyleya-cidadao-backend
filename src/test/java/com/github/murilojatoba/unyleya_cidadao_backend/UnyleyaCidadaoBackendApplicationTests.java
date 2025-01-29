a4910c5e014c1c3ec546fdcd7ebd350120adcc98
Murilo Jatobá <murilo_75517@trt5.jus.br>
2025-01-29 16:14:42 -0300

    Inclusão do projeto importado do Spring Initializr.

---
+++ src/test/java/com/github/murilojatoba/unyleya_cidadao_backend/UnyleyaCidadaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.murilojatoba.unyleya_cidadao_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnyleyaCidadaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
